5cb2b106987eda675da17278954e53067121f326
Rohan Sharma <rohansharma@Rohans-MacBook-Air.local>
2021-03-10 14:21:18 +0530

    commit

--- push.js
+++ push.js
@@ -3,11 +3,14 @@ var push = require('web-push');
 let vapidkeys = {
     publicKey: 'BAtFToO4QoRgNGGFClHmlKmGLnhQ9Xayi4_Ugn4vbzf9TfQfq_qOfEXBQ7bJhUXaeTCWVr0AOSXgH6WW-38fg4A',
     privateKey: 'b8s88XXXcCKUBrRq7XGziBWlQ4NV7SCYrCQMqHjzBm0'
-  }
+  };
 
-push.setVapidDetails('mailto:test@test.com',vapidkeys.publicKey,vapidkeys.privateKey);
+push.setVapidDetails(
+    'mailto:test@test.com',
+    vapidkeys.publicKey,
+    vapidkeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/f_BPV0o-VY4:APA91bFL0CqT3LxajCjNdzhFnBGFikWtGgm3TgyJ8838XjAYdnNIPjRrWQBtTzGpiRAdxBWkk9w-Pk6BhpjcPZpMcf_WVBaPMtfuiubhLH7gta3Fbg5Q6pFVoW9FN4m3QNdARWG78JOj","expirationTime":null,"keys":{"p256dh":"BOyxIDHZ0U13Yvj-G9o75o9wkiG8jxydL8s4SLjMdfDHv0yw6f_cLuj7Z0oWfT7EiAWWVWoh0ibgVxIhAlyk44Y","auth":"CYiSJxKtrDSpAwaE9_kTWQ"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cF_K7vUevF0:APA91bGVy7MOXX9sojA1mSFLPsrJQ-u5hUR3P7r2YywuhQpXtPgVHq_zMMqJJ5BRWUphxqp8K9TSCAPzcA6022G2kKw91mwMriI-eRVYoIvOb9RhmEyWsFi3UOVZcQTzwdj0CXCmF_Ba","expirationTime":null,"keys":{"p256dh":"BNLqRdXzlb7mFjvoG3Q3icdCxQvqAOaq1FMz44-Yr3QbzjGf5E3SMhnICvcrdt4KVf0uciTeZbmZBEBxs8WmoF8","auth":"0eJlCNsdqnZD0o4NBXIL_Q"}}
 push.sendNotification(sub,'test message')
 
 // code to generate VAPID Keys
@@ -15,3 +18,18 @@ push.sendNotification(sub,'test message')
 
 // Console logging Vapid Keys
 // console.log(vapid);
+
+
+// const webpush = require('web-push');
+
+// // VAPID keys should only be generated only once.
+// const vapidKeys = webpush.generateVAPIDKeys();
+
+// webpush.setGCMAPIKey('<Your GCM API Key Here>');
+// webpush.setVapidDetails(
+//   'mailto:example@yourdomain.org',
+//   vapidKeys.publicKey,
+//   vapidKeys.privateKey
+// );
+
+// webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
